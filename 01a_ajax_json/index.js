9d6e5ad0cb7f4c1a180a7e26f71eca1250b68799
carolinadutras <carolina.dutras@gmail.com>
2019-10-29 13:25:47 -0300

    exercicio ajax

--- 01a_ajax_json/index.js
+++ 01a_ajax_json/index.js
@@ -4,6 +4,31 @@
 // [] tratar response
 
 
+// const request = new XMLHttpRequest();
+// const img = document.createElement('img')
+// const div = document.getElementById('root')
+
+// request.onreadystatechange = algumaFuncao;
+// function algumaFuncao() {
+    
+//     console.log('olar')
+//     const response= request.response
+//     const json = JSON.parse(request.response);
+//     console.log(json.data[0].imagem);
+
+//         img.src = json.data[0].imagem;
+//         div.appendChild(img)
+
+
+    
+
+// }
+// const url = './data/dados.json'
+// request.open('GET' , url );
+// request.send()
+
+
+
 const request = new XMLHttpRequest();
 const img = document.createElement('img')
 const div = document.getElementById('root')
