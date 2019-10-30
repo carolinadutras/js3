9792a7b2a7cc38794e4e73f60b79a319d319df0b
=carolinadutras <carolina.dutras@gmail.com>
2019-10-30 17:16:42 -0300

    exercico ajax usando for, insere fotos professoras

--- 01b_ajax_json/index.js
+++ 01b_ajax_json/index.js
@@ -1,10 +1,67 @@
-// [] instanciar XMLHttpRequest()
-// [] o que fazer depois que receber a resposta
-// [] fazer a requisição com open() e send()
-// [] tratar response
-
-
-
 // [] iterar sobre o array e mostrar as três imagens usando for()
 // [] iterar sobre o array e mostrar as três imagens usando for...of
 // [] iterar sobre o array e mostrar as três imagens usando forEach
+
+
+// soluçao 1
+
+// const url = './data/dados.json'
+
+// const request = new XMLHttpRequest;
+
+// request.onreadystatechange = mostraProfessoras;
+
+// request.open('GET', url);
+// request.send();
+
+
+// function mostraProfessoras(){
+//     console.log('entrou na função')
+//     if (request.readyState === 4 && request.status === 200){
+//     const response = request.response;
+//     const json = JSON.parse(response);
+//     console.log(json.data)
+//     const body = document.body;
+    
+//     body.innerHTML += `
+//       <img src=${json.data[0].imagem}>
+//       <img src=${json.data[1].imagem}>
+//       <img src=${json.data[2].imagem}>`
+//   }
+
+
+// }
+
+//solução 2 usando for
+
+const url = './data/dados.json'
+
+const request = new XMLHttpRequest;
+
+request.onreadystatechange = mostraProfessoras;
+
+request.open('GET', url);
+request.send();
+
+
+function mostraProfessoras(){
+    console.log('entrou na função')
+    if (request.readyState === 4 && request.status === 200){
+    const response = request.response;
+    const json = JSON.parse(response);
+    data = json.data
+    const div = document.querySelector('#root');
+        for (let i = 0; i < data.length; i++) {
+            const element = data[i];
+            const img = `<img src = ${element.imagem}>`;
+            div.innerHTML += img
+        }
+
+
+    }
+}
+
+    
+}
+
+
